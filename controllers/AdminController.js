cef540bc03014bf63e70e4c8bb9ef68efc2dd31c
Usman Cheema <140833929+Usman-cheema79@users.noreply.github.com>
2024-05-28 11:22:02 +0500

    .

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const Admin = require("../models/AdminSchema");
 const bcrypt = require("bcrypt");
 
-const saltRounds = 10; // for password hashing, 10 is a good number
+const saltRounds = 10; 
 
-// GET all users
+// GET admins
 router.get("/Admins", async (req, res) => {
   try {
     const Admins = await Admin.find();
@@ -35,46 +35,5 @@ router.post("/Admins", async (req, res) => {
       res.status(400).json({ message: error.message });
     }
   });
-  
-// GET a single user by ID
-router.get("/Admin/:id", getUser, async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const user = await User.findById(id);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    const sanitizedUser = {
-      _id: user._id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-      phone: user.phone,
-      address: user.address,
-      bio: user.bio,
-      imageUrl: user.imageUrl,
-    };
-    res.json(sanitizedUser);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Middleware function to get user by ID
-async function getUser(req, res, next) {
-  let user;
-  try {
-    user = await User.findById(req.params.id);
-    if (user == null) {
-      return res.status(404).json({ message: "User not found" });
-    }
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-
-  res.user = user;
-  next();
-}
 
 module.exports = router;
